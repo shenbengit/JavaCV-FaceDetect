ef753b55e6c86793d82b584460b8ba707a9fd4db
ShenBen <714081644@qq.com>
2021-07-12 15:21:01 +0800

    修改demo事例

--- app/src/main/java/com/shencoder/javacv_facedetectdemo/MainActivity.java
+++ app/src/main/java/com/shencoder/javacv_facedetectdemo/MainActivity.java
@@ -6,7 +6,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Bitmap;
 import android.graphics.Rect;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -16,11 +15,9 @@ import com.shencoder.javacv_facedetect.FaceDetectCameraView;
 import com.shencoder.javacv_facedetect.FaceDetectRequestDialog;
 import com.shencoder.javacv_facedetect.OnFaceDetectListener;
 import com.shencoder.javacv_facedetect.RequestCallback;
+import com.shencoder.javacv_facedetect.RequestDialogLayoutCallback;
 import com.shencoder.javacv_facedetect.util.Nv21ToBitmapUtil;
 
-import org.jetbrains.annotations.NotNull;
-
-import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 
@@ -28,7 +25,6 @@ import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
-import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
     private FaceDetectRequestDialog dialog;
@@ -61,50 +57,83 @@ public class MainActivity extends AppCompatActivity {
                 System.out.println("当前无人--->");
             }
         });
-        dialog = new FaceDetectRequestDialog.Builder(this, Facing.BACK, new RequestCallback() {
-            @Override
-            @NonNull
-            public OkHttpClient.Builder generateOkhttpClient(OkHttpClient.Builder builder) {
-                return builder;
-            }
+        dialog = new FaceDetectRequestDialog.Builder(this, Facing.BACK,
+                new RequestDialogLayoutCallback() {
+                    @Override
+                    public int getLayoutId() {
+                        return R.layout.dialog_face_detect_request;
+                    }
 
-            @NonNull
-            @Override
-            public Request.Builder generateRequest(Request.Builder builder, byte[] data, int width, int height, List<Rect> faceRectList) {
+                    @Override
+                    public int getFaceDetectCameraViewId() {
+                        return R.id.detectCameraView;
+                    }
+
+                    @Override
+                    public void initView(FaceDetectRequestDialog dialog) {
+//                        Button btnClose = dialog.findViewById(R.id.btnShowDialog);
+
+                    }
+
+                    @Override
+                    public void onStart(FaceDetectRequestDialog dialog) {
+
+                    }
+
+                    @Override
+                    public void onStop(FaceDetectRequestDialog dialog) {
+
+                    }
+
+                    @Override
+                    public void onDestroy(FaceDetectRequestDialog dialog) {
+
+                    }
+                },
+                new RequestCallback() {
+                    @Override
+                    @NonNull
+                    public OkHttpClient.Builder generateOkhttpClient(OkHttpClient.Builder builder) {
+                        return builder;
+                    }
+
+                    @NonNull
+                    @Override
+                    public Request.Builder generateRequest(Request.Builder builder, byte[] data, int width, int height, List<Rect> faceRectList) {
 //                Bitmap bitmap = Nv21ToBitmapUtil.nv21ToBitmap(data, width, height);
-                Bitmap bitmap = Nv21ToBitmapUtil.cropNv21ToBitmap(data, width, height, faceRectList.get(0));
-                if (bitmap != null) {
-                    String base64 = Nv21ToBitmapUtil.bitmapToBase64(bitmap, 100);
-                    RequestFaceBean bean = new RequestFaceBean("imagecompare", base64);
-                    RequestBody body = RequestBody.create(MediaType.parse("application/json"), GsonUtil.toJson(bean));
-                    builder.url("http://192.168.2.186:25110")
-                            .post(body);
-                }
-                return builder;
-            }
+                        Bitmap bitmap = Nv21ToBitmapUtil.cropNv21ToBitmap(data, width, height, faceRectList.get(0));
+                        if (bitmap != null) {
+                            String base64 = Nv21ToBitmapUtil.bitmapToBase64(bitmap, 100);
+                            RequestFaceBean bean = new RequestFaceBean("imagecompare", base64);
+                            RequestBody body = RequestBody.create(MediaType.parse("application/json"), GsonUtil.toJson(bean));
+                            builder.url("http://192.168.2.186:25110")
+                                    .post(body);
+                        }
+                        return builder;
+                    }
 
-            @Override
-            public void onRequestStart() {
+                    @Override
+                    public void onRequestStart() {
 
-            }
+                    }
 
-            @Override
-            public void onRequestFailure(Exception e) {
-                System.out.println("网络请求失败：" + e.getMessage());
-                dialog.needRetryDelay(1000L);
-            }
+                    @Override
+                    public void onRequestFailure(Exception e) {
+                        System.out.println("网络请求失败：" + e.getMessage());
+                        dialog.needRetryDelay(1000L);
+                    }
 
-            @Override
-            public void onRequestSuccess(String bodyStr) {
-                ResultBean resultBean = GsonUtil.jsonToBean(bodyStr, ResultBean.class);
-                if (resultBean.getResCode() == 1) {
-                    System.out.println("人脸识别成功:" + resultBean.getData().getUserName());
-                } else {
-                    System.out.println("人脸识别失败");
-                    dialog.needRetryDelay(1000L);
-                }
-            }
-        }).build();
+                    @Override
+                    public void onRequestSuccess(String bodyStr) {
+                        ResultBean resultBean = GsonUtil.jsonToBean(bodyStr, ResultBean.class);
+                        if (resultBean.getResCode() == 1) {
+                            System.out.println("人脸识别成功:" + resultBean.getData().getUserName());
+                        } else {
+                            System.out.println("人脸识别失败");
+                            dialog.needRetryDelay(1000L);
+                        }
+                    }
+                }).build();
         Button button = findViewById(R.id.btnShowDialog);
         button.setOnClickListener(v -> dialog.show());
 
